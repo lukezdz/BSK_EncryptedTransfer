acbe14dc637324e312c346297fa660f93bb303c0
lukezdz <lukas.zdziarski@gmail.com>
2021-06-22 01:40:49 +0200

    Fix connectionThread sending and receiving

--- src/main/java/pl/edu/pg/bsk/transfer/ConnectionThread.java
+++ src/main/java/pl/edu/pg/bsk/transfer/ConnectionThread.java
@@ -4,6 +4,8 @@ import lombok.SneakyThrows;
 import org.json.simple.parser.ParseException;
 import pl.edu.pg.bsk.exceptions.EncryptionFailedException;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -13,10 +15,14 @@ import java.security.spec.InvalidKeySpecException;
 public class ConnectionThread extends Thread {
 	private final TransferHandler handler;
 	private final Socket socket;
+	private final DataOutputStream dos;
+	private final DataInputStream dis;
 
-	public ConnectionThread(TransferHandler handler, Socket socket) {
+	public ConnectionThread(TransferHandler handler, Socket socket) throws IOException {
 		this.handler = handler;
 		this.socket = socket;
+		dos = new DataOutputStream(socket.getOutputStream());
+		dis = new DataInputStream(socket.getInputStream());
 	}
 
 	@SneakyThrows
@@ -25,13 +31,13 @@ public class ConnectionThread extends Thread {
 		System.out.println("Started connection thread for " + socket.getInetAddress());
 		while (!socket.isClosed()) {
 			try {
-				byte[] read = socket.getInputStream().readAllBytes();
+				int length = dis.readInt();
 
-				if (read.length == 0) {
-					continue;
+				if (length > 0) {
+					byte[] read = new byte[length];
+					dis.readFully(read, 0, read.length);
+					handler.receiveData(read, socket.getInetAddress());
 				}
-
-				handler.receiveData(read, socket.getInetAddress());
 			} catch (IOException | ParseException | EncryptionFailedException | NoSuchAlgorithmException | InvalidKeySpecException ignored) {
 				socket.close();
 			}
@@ -40,7 +46,8 @@ public class ConnectionThread extends Thread {
 
 	public void write(byte[] data) throws IOException {
 		System.out.println("Sending data to " + socket.getInetAddress() + ":" + socket.getInetAddress().getHostAddress());
-		socket.getOutputStream().write(data);
+		dos.writeInt(data.length);
+		dos.write(data);
 	}
 
 	public void close() throws IOException {
