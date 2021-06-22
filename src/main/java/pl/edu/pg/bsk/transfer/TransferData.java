d969b51e8f651c5a860b6b6dacb4d50144b70e49
lukezdz <lukas.zdziarski@gmail.com>
2021-06-22 13:30:49 +0200

    Delete unused methods from TransferData

--- src/main/java/pl/edu/pg/bsk/transfer/TransferData.java
+++ src/main/java/pl/edu/pg/bsk/transfer/TransferData.java
@@ -2,14 +2,7 @@ package pl.edu.pg.bsk.transfer;
 
 import lombok.Getter;
 import lombok.Setter;
-import pl.edu.pg.bsk.encryption.EncryptionMode;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.security.PublicKey;
 
@@ -33,59 +26,11 @@ public class TransferData implements Serializable {
 		return new TransferData(metadata, publicKey.getEncoded());
 	}
 
-	public static byte[] getPartTwoHandshakeBody(byte[] secretKey, byte[] iv, EncryptionMode mode) {
-		HandshakeComplexBody body = new HandshakeComplexBody(secretKey, iv, mode);
-
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		ObjectOutputStream out = null;
-		byte[] result = null;
-		try {
-			out = new ObjectOutputStream(bos);
-			out.writeObject(body);
-			out.flush();
-			result = bos.toByteArray();
-		} catch (IOException ex) {
-
-		}
-		finally {
-			try {
-				bos.close();
-			} catch (IOException ex) {
-				// ignore close exception
-			}
-		}
-
-		return result;
-	}
-
 	public static TransferData getPartTwoHandshakeData(byte[] encrypted, Metadata.TransferType transferType) {
 		Metadata metadata = new Metadata(Metadata.MetadataType.HANDSHAKE);
 		metadata.setHandshakePart(2);
 		metadata.setTransferType(transferType);
 
 		return new TransferData(metadata, encrypted);
-	}
-
-	public static HandshakeComplexBody parsePartTwoHandshakeData(byte[] decrypted) {
-		ByteArrayInputStream bis = new ByteArrayInputStream(decrypted);
-		ObjectInput in = null;
-		HandshakeComplexBody body = null;
-		try {
-			in = new ObjectInputStream(bis);
-			 body = (HandshakeComplexBody) in.readObject();
-		} catch (IOException | ClassNotFoundException ex) {
-
-		}
-		finally {
-			try {
-				if (in != null) {
-					in.close();
-				}
-			} catch (IOException ex) {
-				// ignore close exception
-			}
-		}
-
-		return body;
 	}
 }
